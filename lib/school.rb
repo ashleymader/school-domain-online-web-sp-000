318e4c96e7c6f45bb4246277b2aeca245f1713b0
Ashley M Mader <ashley.mader12@gmail.com>
2020-03-06 22:02:57 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,9 +8,23 @@ class School
   def roster
     @roster
   end
-  
-  array = []
-  def add_student(name, grade)
-    array << #{name}, #{grade}
+
+def add_student(name, grade)
+    @student_name = name
+    @grade = grade
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << name
   end
-end 
+
+  def grade(number)
+    @roster[number]
+  end
+
+  def sort
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
+  end
+end
